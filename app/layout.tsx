34a8daff1b67847ef374b0ce5f2b4643f4187245
idulfitridanadha-mmi <idulfitridanadhammi@gmail.com>
2026-05-15 16:04:18 +0700

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,15 @@ const fontUtama = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: "Dashboard Admin - Masjid Manarul Ilmi",
   description: "Sistem Manajemen Qurban RDK 47 ITS",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    shortcut: '/favicon/favicon.ico',
+    apple: '/favicon/apple-touch-icon.png',
+  },
+  manifest: '/favicon/site.webmanifest',
 };
 
 export default function RootLayout({
